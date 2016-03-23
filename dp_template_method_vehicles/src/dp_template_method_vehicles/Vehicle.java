3fcd48403c06ec8b5cc89c6b8a091120f0e54a4c
Jonathon Nordquist <cugamer@gmail.com>
2016-03-23 15:47:06 -0500

    Create Vehicle class.

---
+++ dp_template_method_vehicles/src/dp_template_method_vehicles/Vehicle.java
@@ -0,0 +1,34 @@
+package dp_template_method_vehicles;
+
+public abstract class Vehicle {
+	private String name;
+
+	public final void prepare(){
+		wash();
+		examine();
+		unlock();
+		fuelUp();
+		checkEngine();
+		getLaunchPermission();
+		powerOn();
+	}
+	
+	void wash(){
+		System.out.println(this.name + " is being washed");
+	}
+	
+	void unlock(){
+		System.out.println(this.name + " is now unlocked");
+	}
+	
+	abstract void examine();
+	
+	abstract void powerOn();
+	
+	void fuelUp(){}
+	
+	void checkEngine(){}
+	
+	void getLaunchPermission(){}
+	
+}
